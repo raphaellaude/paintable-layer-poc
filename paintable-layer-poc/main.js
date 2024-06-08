e680346ba45f001e0e7cbc0e1b86ff191cde8d99
Raphael Paul Laude <raphlaude@gmail.com>
2024-06-08 14:52:44 -0400

    basic rendering working
    later learned there's a package that helps do this low level stuff I was messing around with

--- paintable-layer-poc/main.js
+++ paintable-layer-poc/main.js
@@ -1,123 +1,146 @@
-import "./style.css";
-import { tableFromIPC } from "apache-arrow";
-// import initWasm, { readSchema } from "parquet-wasm";
-
-// await initWasm();
-// import { parseField, parseVector } from "arrow-js-ffi";
-
-// const arrowJsFFI = await import(
-//   "https://unpkg.com/arrow-js-ffi@0.2.0/dist/arrow-js-ffi.umd.js"
-// );
-const parquetModule = await import(
-  "https://unpkg.com/parquet-wasm@0.6.0/esm/parquet_wasm.js"
-);
-// Need to await the default export first to initialize the WebAssembly code
-await parquetModule.default();
-
-const geoarrowModule = await import(
-  "https://unpkg.com/geoarrow-wasm@0.1.0/esm/index.js"
-);
-
-// // Need to await the default export first to initialize the WebAssembly code
-const { memory } = await geoarrowModule.default();
-const geoarrow = geoarrowModule[0];
-const geoarrowMemory = geoarrowModule[1];
-
 import { MapboxOverlay as DeckOverlay } from "@deck.gl/mapbox";
-import { SolidPolygonLayer } from "@deck.gl/layers";
+import { SolidPolygonLayer, PathLayer } from "@deck.gl/layers";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./style.css";
+import { tableFromIPC } from "apache-arrow";
 
-// Set your Mapbox token here or via environment variable
-const MAPBOX_TOKEN = import.meta.env.VITE_MapboxAccessToken; // eslint-disable-line
+const loadModules = async () => {
+  const parquetModule = await import(
+    "https://unpkg.com/parquet-wasm@0.6.0/esm/parquet_wasm.js"
+  );
+  await parquetModule.default();
 
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/light-v9",
-  accessToken: MAPBOX_TOKEN,
-  center: [0.45, 51.47],
-  zoom: 4,
-  bearing: 0,
-  pitch: 0,
-});
+  const geoarrowModule = await import(
+    "https://unpkg.com/geoarrow-wasm@0.1.0/esm/index.js"
+  );
+  const { memory } = await geoarrowModule.default();
+  const geoarrow = geoarrowModule[0];
+  const geoarrowMemory = geoarrowModule[1];
 
-map.on("load", () => {
-  const getData = async () => {
-    const parquetPath = "./tl_2023_55_tabblock20.parquet";
-    const parquetBytes = new Uint8Array(
-      await fetch(parquetPath).then((response) => response.arrayBuffer()),
-    );
-    return parquetBytes;
-  };
+  return { parquetModule, geoarrow, geoarrowMemory };
+};
 
-  const parquetBytes = getData();
+const initializeMap = (MAPBOX_TOKEN) => {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/light-v9",
+    accessToken: MAPBOX_TOKEN,
+    center: [-111.536258858, 39.393711569], // Utah
+    // center: [-89.910271927, 44.541378574], // Wisconsin
+    zoom: 7,
+    bearing: 0,
+    pitch: 0,
+  });
 
-  console.log(parquetBytes);
+  return map;
+};
 
-  const decodedArrowBytes = parquetModule.readParquet(parquetBytes, {
+const getData = async () => {
+  const parquetPath = "./Utah.parquet";
+  // const parquetPath = "./wisconsin_blocks.parquet";
+  // const parquetPath = "./tl_2023_55_tabblock20.parquet";
+  const parquetBytes = new Uint8Array(
+    await fetch(parquetPath).then((response) => response.arrayBuffer()),
+  );
+  return parquetBytes;
+};
+
+const processData = async (parquetModule, parquetBytes) => {
+  const decodedArrowBytes = await parquetModule.readParquet(parquetBytes, {
     batchSize: Math.pow(2, 31),
   });
+  // console.log(decodedArrowBytes);
   const arrowTable = tableFromIPC(decodedArrowBytes.intoIPCStream());
+  // console.log(arrowTable);
 
-  console.log(arrowTable);
-  console.log(arrowTable.schema.fields[0]);
+  let geometryColumn = arrowTable.getChildAt(0);
+  // let geometryColumn = arrowTable.getChild("geometry");
 
-  let geometryColumn = arrowTable.getChild("geometry");
+  // console.log(geometryColumn);
+  // console.log(geometryColumn.data[0].valueOffsets);
 
-  console.log(geometryColumn);
+  // const firstGeometry = geometryColumn.get(0);
+  // console.log(firstGeometry);
+  // const firstRing = firstGeometry.get(0);
+  // console.log(firstRing);
+  // const firstCoord = firstRing.get(0);
+  // console.log(firstCoord.toArray());
 
-  // const flatCoordinateVector = geometryColumn
-  //     .getChildAt(0)
-  //     .getChildAt(0)
-  //     .getChildAt(0);
+  // let polygonIndices = geometryColumn.getChildAt(0).data[0].valueOffsets;
+  let polygonIndices = geometryColumn.data[0].valueOffsets;
+  let ringIndices = geometryColumn.getChildAt(0).data[0].valueOffsets;
 
-  // let flatCoordinateArray = flatCoordinateVector.data[0].values;
-  let polygonOffsets = geometryColumn.getChildAt(0).data[0].valueOffsets;
-  let geomOffsets = geometryColumn.data[0].valueOffsets;
-
-  // const coordBuffer = geoarrow.CoordBuffer.from_interleaved_coords(
-  //     new geoarrow.InterleavedCoordBuffer(flatCoordinateArray)
-  //   );
-
-  // const polygonArray = new geoarrow.PolygonArray(
-  //     coordBuffer,
-  //     geomOffsets,
-  //     polygonOffsets
-  //   );
-
-  const resolvedIndices = new Int32Array(geomOffsets.length);
+  const resolvedIndices = new Int32Array(polygonIndices.length);
   for (let i = 0; i < resolvedIndices.length; ++i) {
-    // Perform the lookup into the polygonOffsets array using the geomOffsets array
-    resolvedIndices[i] = polygonOffsets[geomOffsets[i]];
+    // Perform the lookup into the ringIndices array using the polygonIndices array
+    resolvedIndices[i] = ringIndices[polygonIndices[i]];
   }
 
-  const data = {
-    // Number of geometries
-    length: geometryColumn.length,
-    // Indices into coordinateArray where each polygon starts
-    startIndices: resolvedIndices,
-    attributes: {
-      // Flat coordinates array
-      getPolygon: { value: flatCoordinateArray, size: 2 },
-      // Pass in the color values per coordinate vertex
-      getFillColor: { value: colorAttribute, size: 3 },
-    },
-  };
-  const layer = new SolidPolygonLayer({
-    // This is an Observable hack - changing the id will force the layer to refresh when the cell reevaluates
-    id: `layer-${Date.now()}`,
-    data,
-    // Skip normalization for binary data
-    _normalize: false,
-    // Counter-clockwise winding order
-    _windingOrder: "CCW",
-  });
+  //  let coordinateVector = geometryColumn.getChildAt(0).getChildAt(0)
 
-  const deckOverlay = new DeckOverlay({
-    // interleaved: true,
-    layers: [layer],
-  });
+  const flatCoordinateVector = geometryColumn
+    .getChildAt(0)
+    .getChildAt(0)
+    .getChildAt(0);
+  const flatCoordinateArray = flatCoordinateVector.data[0].values;
+  // console.log(flatCoordinateArray);
 
-  map.addControl(deckOverlay);
-  map.addControl(new mapboxgl.NavigationControl());
-});
+  return { arrowTable, resolvedIndices, flatCoordinateArray };
+};
+
+const main = async () => {
+  const { parquetModule, geoarrow, geoarrowMemory } = await loadModules();
+
+  const MAPBOX_TOKEN = import.meta.env.VITE_MapboxAccessToken;
+  const map = initializeMap(MAPBOX_TOKEN);
+
+  map.on("load", async () => {
+    const parquetBytes = await getData();
+    const { arrowTable, resolvedIndices, flatCoordinateArray } =
+      await processData(parquetModule, parquetBytes);
+
+    // Fast
+    const layer = new SolidPolygonLayer({
+      id: `layer-${Date.now()}`,
+      data: {
+        length: arrowTable.numRows,
+        startIndices: resolvedIndices,
+        attributes: {
+          getPolygon: { value: flatCoordinateArray, size: 2 },
+        },
+      },
+      _normalize: false,
+      _windingOrder: "CCW",
+      getFillColor: [0, 100, 60, 160],
+      // pickable: true,
+    });
+
+    // Slow
+    // const outline = new PathLayer({
+    //   id: `layer-line-${Date.now()}`,
+    //   data: {
+    //     length: arrowTable.numRows,
+    //     startIndices: resolvedIndices,
+    //     attributes: {
+    //       getPath: { value: flatCoordinateArray, size: 2 },
+    //     },
+    //   },
+    //   getColor: [80, 80, 80],
+    //   getWidth: 1,
+    //   widthMinPixels: 1,
+    //   widthMaxPixels: 4,
+    //   jointRounded: true,
+    //   _pathType: "loop",
+    // });
+
+    const deckOverlay = new DeckOverlay({
+      layers: [layer],
+    });
+
+    map.addControl(deckOverlay);
+    map.addControl(new mapboxgl.NavigationControl());
+  });
+};
+
+main();
