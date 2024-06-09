cac6bd5660015a885f65086d0f258f9d01cd1b29
Raphael Paul Laude <raphlaude@gmail.com>
2024-06-09 11:48:35 -0400

    working faster but state isnt propagated across clients correctly

--- districtr-merge/src/components/Map.tsx
+++ districtr-merge/src/components/Map.tsx
@@ -1,24 +1,30 @@
-import { useEffect, useRef } from "react";
-import { AutomergeUrl, RawString } from "@automerge/automerge-repo";
+import { useEffect, useRef, useState } from "react";
+import { AutomergeUrl } from "@automerge/automerge-repo";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { Protocol } from "pmtiles";
 import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import { Plan } from "../App";
 
-// const colorMap: string[] = ["#2090FF", "#ffff22", "#cccccc"]
-//
 interface Point {
   x: number;
   y: number;
 }
 
+function boxAround(point: Point, radius: number) {
+  const southwest = [point.x + radius, point.y + radius];
+  const northeast = [point.x - radius, point.y - radius];
+  return [northeast, southwest];
+}
+
 export default function Map({ docUrl }: { docUrl: AutomergeUrl }) {
   const [doc, changeDoc] = useDocument<Plan>(docUrl);
+  const [initialized, setInitialized] = useState(false);
   const mapContainer = useRef(null);
   const map = useRef(null);
 
   useEffect(() => {
+    setInitialized(false);
     const protocol = new Protocol();
     maplibregl.addProtocol("pmtiles", protocol.tile);
 
@@ -30,6 +36,7 @@ export default function Map({ docUrl }: { docUrl: AutomergeUrl }) {
           wisconsin_blocks: {
             type: "vector",
             url: "pmtiles:///wisconsin_blocks.pmtiles",
+            promoteId: "geoid",
           },
         },
         layers: [
@@ -43,12 +50,55 @@ export default function Map({ docUrl }: { docUrl: AutomergeUrl }) {
         ],
       },
       center: [-89.6787214, 44.3140374],
-      zoom: 6,
+      zoom: 9,
       maxZoom: 20.9,
-      minZoom: 5,
+      minZoom: 9,
     });
 
     map.current.on("load", () => {
+      const landmarkPaintProperty = {
+        "fill-opacity": [
+          "case",
+          ["boolean", ["feature-state", "hover"], false],
+          0.6,
+          0,
+        ],
+        "fill-color": "#aaa",
+      };
+
+      map.current.addLayer({
+        id: "blocks-hover",
+        type: "fill",
+        layout: {},
+        source: "wisconsin_blocks",
+        "source-layer": "tl_2023_55_tabblock20",
+        paint: landmarkPaintProperty,
+      });
+
+      map.current.addLayer({
+        id: "blocks-fill",
+        type: "fill",
+        source: "wisconsin_blocks",
+        "source-layer": "tl_2023_55_tabblock20",
+        paint: {
+          "fill-color": [
+            "match",
+            ["feature-state", "color"],
+            0,
+            "#0099cd",
+            1,
+            "#ffca5d",
+            2,
+            "#00cd99",
+            3,
+            "#99cd00",
+            4,
+            "#cd0099",
+            "#efefef",
+          ],
+        },
+      });
+
       map.current.addLayer({
         id: "blocks",
         type: "line",
@@ -59,91 +109,99 @@ export default function Map({ docUrl }: { docUrl: AutomergeUrl }) {
           "line-width": 1,
         },
       });
-
-      const landmarkPaintProperty = {
-        "fill-opacity": 0.5, // turn off the highlights by default
-        "fill-color": [
-          "case",
-          ["boolean", ["feature-state", "hover"], false],
-          "#ff4f49",
-          "#e44944",
-        ],
-      };
-
-      map.current.addLayer({
-        id: "blocks-hover",
-        type: "fill",
-        source: "wisconsin_blocks",
-        "source-layer": "tl_2023_55_tabblock20",
-        paint: landmarkPaintProperty,
-      });
     });
 
-    function boxAround(point: Point, radius: number) {
-      const southwest = [point.x + radius, point.y + radius];
-      const northeast = [point.x - radius, point.y - radius];
-      return [northeast, southwest];
-    }
+    // function onMouseMove(e, hover = true) {
+    //   console.log(e.feature);
+    //   const box = boxAround(e.point, 4);
+    //   const features = map.current.queryRenderedFeatures(box, {
+    //     layers: ["blocks-hover"],
+    //   });
+    //   if (features.length > 0) {
+    //     features.forEach((feat) => {
+    //       // console.log(feat.id, feat.state);
+    //       map.current.setFeatureState(
+    //         {
+    //           source: "wisconsin_blocks",
+    //           sourceLayer: "tl_2023_55_tabblock20",
+    //           id: feat.id,
+    //         },
+    //         {
+    //           ...feat.state,
+    //           hover: hover,
+    //         },
+    //       );
+    //     });
+    //     // console.log(features.length);
+    //     // console.log(features.map((f) => f.properties.geoid));
+    //   }
+    // }
 
-    function onMouseMove(e) {
-      const box = boxAround(e.point, 4);
-      const features = map.current.queryRenderedFeatures(box, {
-        layers: ["blocks"],
-      });
-      if (features.length > 0) {
-        // features.forEach((feat) => {
-        //   map.current.setFeatureState(feat.id, {
-        //     ...feat.state,
-        //     hover: true,
-        //   });
-        // });
-        console.log(features.length);
-        // console.log(features.map((f) => f.properties.geoid));
-      }
-    }
-
-    map.current.on("mousemove", onMouseMove);
-
-    function addAssignments(features) {
-      console.log("adding assignments", doc);
-      changeDoc((d) => {
-        const key = doc.name.toString();
-        console.log(key);
-
-        features.forEach((feat) => {
-          const key = feat.properties.geoid;
-          const currentAssignment = d.assignments[key];
-          console.log(currentAssignment);
-
-          if (currentAssignment !== undefined) {
-            d.assignments[key] = currentAssignment + 1;
-          } else {
-            d.assignments[key] = 1;
-          }
-        });
-      });
-    }
-
-    function onMouseClick(e) {
-      const box = boxAround(e.point, 4);
-      const features = map.current.queryRenderedFeatures(box, {
-        layers: ["blocks"],
-      });
-      if (features.length > 0) {
-        console.log(features);
-        addAssignments(features);
-      }
-    }
-
-    map.current.on("click", onMouseClick);
+    // map.current.on("mousemove", "blocks-hover", onMouseMove);
+    // map.current.on("mouseleave", "blocks-hover", (e) => onMouseMove(e, false));
 
     map.current.addControl(new maplibregl.NavigationControl());
+
+    // map.current.on("zoom", () => {
+    //   console.log(map.current.getZoom());
+    // });
+
+    map.current.on("click", "blocks-fill", (e) => {
+      const box = boxAround(e.point, 50);
+      const features = map.current.queryRenderedFeatures(box, {
+        layers: ["blocks-fill"],
+      });
+
+      const uniqueFeatures: Set<string> = new Set(
+        features.map((f) => f.properties.geoid),
+      );
+
+      if (uniqueFeatures) {
+        changeDoc((d) => {
+          uniqueFeatures.forEach((key) => {
+            const assignment = d.assignments[key];
+            const newAssignment =
+              assignment !== undefined ? d.assignments[key] + 1 : 0;
+            d.assignments[key] = newAssignment;
+            map.current.setFeatureState(
+              {
+                source: "wisconsin_blocks",
+                sourceLayer: "tl_2023_55_tabblock20",
+                id: key,
+              },
+              {
+                // ...feat.state,
+                color: newAssignment % 5,
+              },
+            );
+          });
+        });
+      }
+    });
 
     return () => {
       maplibregl.removeProtocol("pmtiles");
       map.current.remove();
     };
   }, []);
+
+  useEffect(() => {
+    if (doc && map && !initialized) {
+      Object.entries(doc.assignments).forEach(([key, value]) => {
+        map.current.setFeatureState(
+          {
+            source: "wisconsin_blocks",
+            sourceLayer: "tl_2023_55_tabblock20",
+            id: key,
+          },
+          {
+            color: value % 5,
+          },
+        );
+      });
+      setInitialized(true);
+    }
+  }, [doc, map, initialized, setInitialized]);
 
   return (
     <>
